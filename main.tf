0099ac3db18729266ff3da58f7d13d22c9e94965
Mukesh <mukrawat007@outlook.com>
2022-06-28 00:38:13 +0530

    feat: add vpc configs

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-module "management-vpc" {
+module "vpc" {
 #  for_each  = toset(["management", "production", "development"])
 #  namespace = "${each.key}"
-  source = "./management-vpc"
+  source = "./vpc"
   cidr   = "10.0.0.0/16"
   subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
   avail_zone = ["us-east-1a", "us-east-1b"]
   private_avail_zone = ["us-east-1c", "us-east-1d"]
   global_ip = "0.0.0.0/0"
-  namespace = "production"
+  namespace = "development"
 }
 
 module "vpc-configs" {
 #   for_each  = toset(["management", "production", "development"])
 #   namespace = "${each.key}"
    source = "./vpc-configs"
-   vpc_id = "${module.management-vpc.vpc_id}" 
+   vpc_id = "${module.vpc.vpc_id}" 
    aws_region = var.aws_region
-   namespace = "production"
+   namespace = "development"
 }
